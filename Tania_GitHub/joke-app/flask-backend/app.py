1e23f0023894273b242b685b1c9e52ccb86ec5ad
Tania <mstaniadzumba@gmail.com>
2025-08-18 23:25:33 +0200

    Add endpoint for fetching random joke from external API

--- Tania_GitHub/joke-app/flask-backend/app.py
+++ Tania_GitHub/joke-app/flask-backend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import requests
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +11,19 @@ def home():
 
 @app.route("/joke")
 def get_joke():
-    return jsonify({"joke":"Why don’t scientists trust atoms? Because they make up everything!" })
+
+    try:
+        response = requests.get("https://official-joke-api.appspot.com/random_joke")
+        response.raise_for_status()#check for http erros
+        joke_data = response.json()
+        return jsonify({
+            "setup": joke_data["setup"],
+            "punchline": joke_data["punchline"]
+            })
+
+    except requests.RequestException:
+            return jsonify({"error": "could not fetch a joke at this time"}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True)
